f699c014374fcf6f9cefb68fc2db16b84e467b01
Warren Uniewski <warren@autimo.com>
2020-12-03 21:46:30 -0800

    update tf for tf cloud

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,5 +1,14 @@
 data "aws_caller_identity" "current" {}
 
+provider "aws" {
+  region  = var.aws_region
+  version = "~> 2.64"
+
+  assume_role {
+    role_arn = "arn:aws:iam::${local.account_id}:role/BCGOV_${var.account_name}_Automation_Admin_Role"
+  }
+}
+
 locals {
   account_id = data.aws_caller_identity.current.account_id
 
@@ -9,13 +18,4 @@ locals {
     Environment = "Development"
   }
   create_ecs_service = var.client_app_image == "" ? 0 : 1
-}
-
-provider "aws" {
-  region  = var.aws_region
-  version = "~> 2.64"
-
-  assume_role {
-    role_arn = "arn:aws:iam::${locals.account_id}:role/BCGOV_${var.account_name}_Automation_Admin_Role"
-  }
 }
