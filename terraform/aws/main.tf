303e3c43d196bbd67881567673ea7356f0cdd640
Warren Uniewski <warren@autimo.com>
2020-12-04 16:09:02 -0800

    update PR with tf plan output

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   version = "~> 3.11"
 
   assume_role {
-    role_arn = "arn:aws:iam::${var.target_aws_account_id}:role/BCGOV_${var.target_env}_Automation_Admin_Role"
+    role_arn = "arn:aws:iam::${var.target_aws_account_id[var.target_env]}:role/BCGOV_${var.target_env}_Automation_Admin_Role"
   }
 }
 
