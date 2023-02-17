95ddcf4ceeec50e516befc1ec3920fbf6af95ac6
Carles Roch-Cunill <45524596+crochcunill@users.noreply.github.com>
2023-02-17 11:40:09 -0800

    Updating greeting for u1mx8l

--- client/src/components/form/Greeter.js
+++ client/src/components/form/Greeter.js
@@ -41,7 +41,7 @@ console.log(values)
             options={[
               { value: 'Aloha', label: 'Aloha' },
               { value: 'Bonjour', label: 'Bonjour' },
-              { value: 'Containers', label: 'Upgrade to 1.5.5- Containers-  szlw3p Feb 9, 2023' },
+              { value: 'Containers', label: 'Upgrade to 1.5.5- Containers-  u1mx8l Feb 17, 2023' },
               { value: 'Greetings and salutations', label: 'Greetings and salutations' },
               { value: 'Hello', label: 'Hello' },
               { value: 'Howdy', label: 'Howdy' },
