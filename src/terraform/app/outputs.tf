14e75cbb83e7ef4cf72ff60fab9c549f30d7677f
Warren Uniewski <warren@autimo.com>
2023-09-27 11:33:39 -0700

    update outputs

--- src/terraform/app/outputs.tf
+++ src/terraform/app/outputs.tf
@@ -1,10 +1,19 @@
 # outputs.tf
 
-output "api_gateway_url" {
-  value = aws_apigatewayv2_api.app.api_endpoint
+output "apigw_url" {
+  description = "Base URL for API Gateway stage."
+  value       = aws_apigatewayv2_api.app.api_endpoint
 }
 
-output "cloudfront_url" {
-  value = "https://${aws_cloudfront_distribution.s3_distribution.domain_name}"
+output "cloudfront" {
+  description = "CloudFront distribution."
+  value = {
+    domain_name     = aws_cloudfront_distribution.s3_distribution.domain_name
+    distribution_id = aws_cloudfront_distribution.s3_distribution.id
+  }
+}
 
+output "s3_bucket_arn" {
+  description = "ARN of S3 bucket for storing static assets."
+  value       = aws_s3_bucket.site.arn
 }
